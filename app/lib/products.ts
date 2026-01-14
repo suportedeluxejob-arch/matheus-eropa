2d224f666dbbf84b882a74fd478262c1894f0ec2
suportedeluxejob-arch <suporte.deluxejob@gmail.com>
2026-01-14 19:48:10 -0300

    first

--- app/lib/products.ts
+++ app/lib/products.ts
@@ -148,7 +148,7 @@ export const PRODUCTS: Product[] = [
         descriptionKey: "descriptions.champions_league_desc",
         priceKey: "product.price_europe_standard",
         oldPriceKey: "product.price_europe_standard_old",
-        image: "/eafc26-bundle-promo.png",
+        image: "https://iili.io/fSJQIEX.md.png",
         link: "https://go.invictuspay.app.br/3p65wtvjht",
         displayCategory: "europe",
         isPromo: true,
